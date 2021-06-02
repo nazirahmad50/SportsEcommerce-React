03f308e3da535f700dc5bc8066a24810e82c178e
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-02 18:01:00 +0100

    Add basic data to consume for web service

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+  return {
+    categories: ["Watersports", "Soccer", "Chess"],
+    products: [
+      { id: 1, name: "Water board", category: "Watersports" },
+      { id: 2, name: "Jacket", category: "Watersports" },
+      { id: 3, name: "Surfer", category: "Watersports" },
+      { id: 4, name: "Ball", category: "Soccer" },
+      { id: 5, name: "Stadium", category: "Soccer" },
+      { id: 6, name: "Player", category: "Soccer" },
+      { id: 7, name: "Chess board", category: "Chess" },
+      { id: 8, name: "King play", category: "Chess" },
+      { id: 9, name: "Queen play", category: "Chess" },
+    ],
+    orders: [],
+  };
+};
