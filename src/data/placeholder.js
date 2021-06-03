82d77a120b8cecb89c45f88aa8e66cf447538d30
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-03 17:52:38 +0100

    Add placeholder data

---
+++ src/data/placeholder.js
@@ -0,0 +1,40 @@
+export const data = {
+  categories: ["Watersports", "Soccer", "Chess", "Running"],
+  products: [
+    {
+      id: 1,
+      name: "Water board",
+      category: "Watersports",
+      description: "a water board used for sport",
+      price: 89,
+    },
+    {
+      id: 2,
+      name: "Jacket",
+      category: "Watersports",
+      description: "water proof jacket",
+      price: 220.78,
+    },
+    {
+      id: 3,
+      name: "Surfer",
+      category: "Watersports",
+      description: "surfs on top of water",
+      price: 999,
+    },
+    {
+      id: 4,
+      name: "Gold ball",
+      category: "Soccer",
+      description: "Ball that will break your feet",
+      price: 69.69,
+    },
+    {
+      id: 5,
+      name: "Stadium",
+      category: "Soccer",
+      description: "statiumd to play footy in",
+      price: 111.11,
+    },
+  ],
+};
