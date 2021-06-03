609672ab7580bf483b88faa50331f3c249a3352c
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-03 18:08:43 +0100

    Create product list comp

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,11 +1,10 @@
 import { ActionTypes } from "./Types";
-import {data phData} from "./placeholder";
+import { data as phData } from "./placeholder";
 
-
-export const loadData = (dataType) =>({
-    type:ActionTypes.DATA_LOAD,
-    payload:{
-        dataType:dataType,
-        data:phData[dataType]
-    }
-})
+export const loadData = (dataType) => ({
+  type: ActionTypes.DATA_LOAD,
+  payload: {
+    dataType: dataType,
+    data: phData[dataType],
+  },
+});
