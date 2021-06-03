76f83e20058e0840804bb79edc809250a3436d2f
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-03 18:13:48 +0100

    Create Category navigation comp

---
+++ src/shop/CategoryNavigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class CategoryNavigation extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Link className="btn btn-secondary btn-block" to={this.props.baseUrl}>
+          All
+        </Link>
+        {this.props.categories &&
+          this.props.categories.map((c) => (
+            <Link
+              className="btn btn-secondary btn-block"
+              key={c}
+              to={`${this.props.baseUrl}/${c.lowerCase()}`}
+            >
+              {c}
+            </Link>
+          ))}
+      </React.Fragment>
+    );
+  }
+}
