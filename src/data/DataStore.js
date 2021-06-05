f4755c94fb74666a55e72edbca1b4f58540ee7e3
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-05 14:26:40 +0100

    Create common reducer

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,4 +1,8 @@
 import { createStore } from "redux";
+import { CommonReducer } from "./CommonReducer";
 import { ShopReducer } from "./ShopReducer";
+import { CartReducer } from "./CartReducer";
 
-export const SportsEcommerceDataStore = createStore(ShopReducer);
+export const SportsEcommerceDataStore = createStore(
+  CommonReducer(ShopReducer, CartReducer)
+);
