7ef275c190cdde51f157391d5321af6c23aa9a8f
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-06 15:56:11 +0100

    Use faker lib to generate fake data for service

--- data.js
+++ data.js
@@ -1,71 +1,23 @@
+var faker = require("faker");
+var data = [];
+var categories = ["Watersports", "Soccer", "Chess", "Running"];
+faker.seed(100);
+
+for (let i = 0; i <= 503; i++) {
+  var category = faker.helpers.randomize(categories);
+  data.push({
+    id: i,
+    name: faker.commerce.productName(),
+    category: category,
+    description: `${category}: ${faker.lorem.sentence(3)}`,
+    price: Number(faker.commerce.price()),
+  });
+}
+
 module.exports = function () {
   return {
-    categories: ["Watersports", "Soccer", "Chess"],
-    products: [
-      {
-        id: 1,
-        name: "Water board",
-        category: "Watersports",
-        description: "a water board used for sport",
-        price: 89,
-      },
-      {
-        id: 2,
-        name: "Jacket",
-        category: "Watersports",
-        description: "water proof jacket",
-        price: 220.78,
-      },
-      {
-        id: 3,
-        name: "Surfer",
-        category: "Watersports",
-        description: "surfs on top of water",
-        price: 999,
-      },
-      {
-        id: 4,
-        name: "Gold ball",
-        category: "Soccer",
-        description: "Ball that will break your feet",
-        price: 69.69,
-      },
-      {
-        id: 5,
-        name: "Stadium",
-        category: "Soccer",
-        description: "statiumd to play footy in",
-        price: 111.11,
-      },
-      {
-        id: 6,
-        name: "Player",
-        category: "Soccer",
-        description: "Ronaldo",
-        price: 999999.99,
-      },
-      {
-        id: 7,
-        name: "Chess board",
-        category: "Chess",
-        description: "chess board to play chess on",
-        price: 334,
-      },
-      {
-        id: 8,
-        name: "King play",
-        category: "Chess",
-        description: "king player",
-        price: 22,
-      },
-      {
-        id: 9,
-        name: "Queen play",
-        category: "Chess",
-        description: "Queen player",
-        price: 33,
-      },
-    ],
+    categories: categories,
+    products: data,
     orders: [],
   };
 };
