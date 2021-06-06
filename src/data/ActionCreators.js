9838222d99930b7f789f5322fbd4d99ca729fa43
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-06 15:48:36 +0100

    -Create async middleware and apply it to the store - update action creators to use rest data source

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,10 +1,12 @@
 import { ActionTypes } from "./Types";
-import { data as phData } from "./placeholder";
+// import { data as phData } from "./placeholder";
+import { RestDataSource } from "./RestDataSource";
+
+const dataSource = new RestDataSource();
 
 export const loadData = (dataType) => ({
   type: ActionTypes.DATA_LOAD,
-  payload: {
-    dataType: dataType,
-    data: phData[dataType],
-  },
+  payload: dataSource
+    .getData(dataType)
+    .then((res) => ({ dataType, data: res.data })),
 });
--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { CommonReducer } from "./CommonReducer";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
+import { asynActions } from "./AsyncMiddleware";
 
 export const SportsEcommerceDataStore = createStore(
-  CommonReducer(ShopReducer, CartReducer)
+  CommonReducer(ShopReducer, CartReducer),
+  applyMiddleware(asynActions)
 );
