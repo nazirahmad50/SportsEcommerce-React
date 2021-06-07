ea88bcf6b74dcb7c6b6acabcbeeb31a8b3721877
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-07 18:23:18 +0100

    Update rest data source to take params

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -2,7 +2,8 @@ import Axios from "axios";
 import { RestUrls } from "./Urls";
 
 export class RestDataSource {
-  getData = (dataType) => this.SendRequest("get", RestUrls[dataType]);
+  getData = async (dataType, params) =>
+    this.SendRequest("get", RestUrls[dataType], params);
 
-  SendRequest = (method, url) => Axios.request({ method, url });
+  SendRequest = (method, url, params) => Axios.request({ method, url, params });
 }
