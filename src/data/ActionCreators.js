69ba6a74fdbe842956d2d92174bff6a46b7dd452
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-09 17:06:36 +0100

    - Add types and action creators for pagination - Update shop reducer to use pagination

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -6,12 +6,20 @@ const dataSource = new RestDataSource();
 
 export const loadData = (dataType, params) => ({
   type: ActionTypes.DATA_LOAD,
-  payload: dataSource
-    .getData(dataType, params)
-    .then((res) => ({
-      dataType,
-      data: res.data,
-      total: Number(res.headers["x-total-count"]),
-      params,
-    })),
+  payload: dataSource.getData(dataType, params).then((res) => ({
+    dataType,
+    data: res.data,
+    total: Number(res.headers["x-total-count"]),
+    params,
+  })),
+});
+
+export const setPageSize = (newSize) => ({
+  type: ActionTypes.DATA_SET_PAGESIZE,
+  payload: newSize,
+});
+
+export const setSortProperty = (newprop) => ({
+  type: ActionTypes.DATA_SET_SORT_PROPERTY,
+  payload: newprop,
 });
